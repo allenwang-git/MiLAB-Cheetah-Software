07f231c1a523520713ad7d5b1d98c265bcd3c84b
YinuoWang <dbdxwyn@163.com>
2021-03-15 21:20:20 +0800

    0315wyn8 modify initial joint pose lying on the ground

--- sim/src/Simulation.cpp
+++ sim/src/Simulation.cpp
@@ -98,73 +98,51 @@ Simulation::Simulation(RobotType robot, Graphics3D* window,
   FBModelState<double> x0;
   x0.bodyOrientation = rotationMatrixToQuaternion(
       ori::coordinateRotation(CoordinateAxis::Z, 0.));
-  // Mini Cheetah
   x0.bodyPosition.setZero();
   x0.bodyVelocity.setZero();
   x0.q = zero12;
   x0.qd = zero12;
+  // Initial (lies on the ground )
+  if (_robot == RobotType::MILAB){
+      x0.bodyPosition[2] = 0.0752;
+      x0.q[0] = -0.5;
+      x0.q[1] = 1.25;
+      x0.q[2] = -2.8;
 
-  /*Mini Cheetah Initial Posture
-   x0.bodyPosition[2] = -0.49;
-   Cheetah 3
-   x0.bodyPosition[2] = -0.34;
-   x0.q[0] = -0.807;
-   x0.q[1] = -1.2;
-   x0.q[2] = 2.4;
+      x0.q[3] = 0.5;
+      x0.q[4] = 1.25;
+      x0.q[5] = -2.8;
 
-   x0.q[3] = 0.807;
-   x0.q[4] = -1.2;
-   x0.q[5] = 2.4;
+      x0.q[6] = -0.5;
+      x0.q[7] = 1.25;
+      x0.q[8] = -2.8;
 
-   x0.q[6] = -0.807;
-   x0.q[7] = -1.2;
-   x0.q[8] = 2.4;
+      x0.q[9] = 0.5;
+      x0.q[10] = 1.25;
+      x0.q[11] = -2.8;
+  } else{  //  MiNI CHEETAH & CHEETAH 3
+      x0.bodyPosition[2] = 0.05;
+      x0.q[0] = -0.7;
+      x0.q[1] = -1.;
+      x0.q[2] = 2.715;
 
-   x0.q[9] = 0.807;
-   x0.q[10] = -1.2;
-   x0.q[11] = 2.4;
+      x0.q[3] = 0.7;
+      x0.q[4] = -1.;
+      x0.q[5] = 2.715;
 
-   Initial (Mini Cheetah stand)
-   x0.bodyPosition[2] = -0.185;
-   Cheetah 3
-   x0.bodyPosition[2] = -0.075;
+      x0.q[6] = -0.7;
+      x0.q[7] = -1.0;
+      x0.q[8] = 2.715;
 
-   x0.q[0] = -0.03;
-   x0.q[1] = -0.79;
-   x0.q[2] = 1.715;
-
-   x0.q[3] = 0.03;
-   x0.q[4] = -0.79;
-   x0.q[5] = 1.715;
-
-   x0.q[6] = -0.03;
-   x0.q[7] = -0.72;
-   x0.q[8] = 1.715;
-
-   x0.q[9] = 0.03;
-   x0.q[10] = -0.72;
-   x0.q[11] = 1.715;
-*/
-  // Initial (Mini Cheetah lies on the ground )
-  //  x0.bodyPosition[2] = 0.05;
-  //  Milab robot
-  x0.bodyPosition[2]=0.076;
-  x0.q[0] = -0.7;
-  x0.q[1] = 1.;
-  x0.q[2] = 2.715;
-
-  x0.q[3] = 0.7;
-  x0.q[4] = 1.;
-  x0.q[5] = 2.715;
-
-  x0.q[6] = -0.7;
-  x0.q[7] = -1.0;
-  x0.q[8] = -2.715;
-
-  x0.q[9] = 0.7;
-  x0.q[10] = -1.0;
-  x0.q[11] = -2.715;
-  x0.q.setZero();
+      x0.q[9] = 0.7;
+      x0.q[10] = -1.0;
+      x0.q[11] = 2.715;
+      if (_robot==RobotType::CHEETAH_3){
+          x0.bodyPosition[2] = 0.075;
+      }
+  }
+//  Bebug
+//  x0.q.setZero();
 
   setRobotState(x0);
   _robotDataSimulator->setState(x0);
